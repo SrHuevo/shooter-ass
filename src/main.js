e9cd392730d77748ae62f4ce5626e7fb0ff4c751
Samuel Escapa Otani <samuel.escapa@intelygenz.com>
2017-07-25 17:16:15 +0200

    Physics

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
 // This begins the creation of a function that we will 'call' just after it's built
 var createScene = function (camera) {
-    // Now create a basic Babylon Scene object 
+    // Now create a basic Babylon Scene object
     var scene = new BABYLON.Scene(engine);
     // Change the scene background color to green.
     scene.clearColor = new BABYLON.Color3(1, 1, 1);
     // This creates and positions a free camera
     var camera = new BABYLON.VRDeviceOrientationFreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
     var camera2 = new BABYLON.WebVRFreeCamera("camera2", new BABYLON.Vector3(0, 5, -10), scene);
+    camera.checkCollisions = true;
+    camera.applyGravity = true;
     // This targets the camera to scene orig
     camera.setTarget(BABYLON.Vector3.Zero());
     camera2.setTarget(BABYLON.Vector3.Zero());
     // This attaches the camera to the canvas
     camera.attachControl(canvas, false);
     camera2.attachControl(canvas, false);
-    
+
     var vr = true;
     document.getElementById("changeCamera").addEventListener( 'click', function () {
         scene.activeCamera = vr ? camera2 : camera;
@@ -24,19 +26,24 @@ var createScene = function (camera) {
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
     // Dim the light a small amount
     light.intensity = .5;
-    
+
     createSkybox(scene);
-    
+
     // Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
     var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
     // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-    
+    sphere.position.y = 10;
+
     var texture1 = new BABYLON.Texture("textures/grass.png", scene);
 
     // Let's try our built-in 'ground' shape.  Params: name, width, depth, subdivisions, scene
     var ground = BABYLON.Mesh.CreateGround("ground1", 100, 100, 24, scene);
     ground.material = new BABYLON.StandardMaterial("texture1", scene);
+    ground.checkCollisions = true;
+    scene.enablePhysics(null, new BABYLON.OimoJSPlugin());
+
+    sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1 }, scene);
+    ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0.5, restitution: 1 }, scene);
     // Leave this function
     return scene;
 };  // End of createScene function
